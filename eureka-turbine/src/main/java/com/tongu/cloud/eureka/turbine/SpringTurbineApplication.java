a612cc085ad2849c3ff5da51b089f1ba5c8fc348
xieqiang <xieqiang@192.168.2.104>
2018-04-21 19:50:06 +0800

    新增项目eureka－turbine

---
+++ eureka-turbine/src/main/java/com/tongu/cloud/eureka/turbine/SpringTurbineApplication.java
@@ -0,0 +1,18 @@
+package com.tongu.cloud.eureka.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableTurbine
+@EnableDiscoveryClient
+public class SpringTurbineApplication 
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTurbineApplication.class, args);
+	}
+}
